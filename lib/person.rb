ea755978ae659bf20c0e757b1bbfeb7e44281e2e
Erin Ponsonby <erinponsonby@gmail.com>
2019-04-24 00:46:16 +0000

    Done.

---
+++ lib/person.rb
@@ -0,0 +1,18 @@
+class Person
+  
+  def name=(person_name)
+    @name = person_name
+  end
+  
+  def name
+    @name
+  end
+  
+  def job=(name_of_job)
+    @job = name_of_job
+  end
+  
+  def job
+    @job
+  end
+end
